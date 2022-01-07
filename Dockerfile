6f4329ac9aab9da83c1233a1a937fbd3a04cf10e
Vasilis Papaleonidas <scoufman@gmail.com>
2022-01-07 16:26:48 +0200

    1.0.6
    - Smaller docker image size.
    - Updated libraries versions.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,7 @@
-FROM node:11.12-slim 
-RUN apt-get update
-RUN apt-get install -y vim
+FROM mhart/alpine-node:13.12.0
 
 WORKDIR /etc/opt
 COPY ./sources/package.json /etc/opt/package.json
-RUN cd /etc/opt/
 RUN npm i
 COPY ./sources/ws_server.js /etc/opt/ws_server.js
-RUN ls -l /etc/opt/
-CMD node /etc/opt/ws_server.js
+CMD while true; do node ws_server; sleep 5; done
--- sources/ws_server.js
+++ sources/ws_server.js
@@ -1,29 +1,29 @@
-var log4js = require('log4js');
+var log4js = require("log4js");
 var logger = log4js.getLogger();
-logger.level = 'info';
+logger.level = "info";
 
 log4js.configure({
   appenders: {
-    filelog: { type: 'dateFile', filename: './logs/ws_server.log' },
-    everything: { type: 'stdout' },
+    filelog: { type: "dateFile", filename: "./logs/ws_server.log" },
+    everything: { type: "stdout" },
   },
-  categories: { default: { appenders: ['filelog', 'everything'], level: 'info' } }
+  categories: { default: { appenders: ["filelog", "everything"], level: "info" } },
 });
 
-const WebSocket = require('ws')
-var mqtt = require('mqtt')
-var VERSION = "1.0.5"
-var messages = 0
+const WebSocket = require("ws");
+var mqtt = require("mqtt");
+var VERSION = "1.0.6";
+var messages = 0;
 
-logger.info("Starting WS Server:"+VERSION)
-var ws_config = JSON.parse(process.env.CONFIG.replace(/'/g,"\""))
-logger.info("CONFIG")
-logger.info(ws_config)
+logger.info("Starting WS Server:" + VERSION);
+var ws_config = JSON.parse(process.env.CONFIG.replace(/'/g, '"'));
+logger.info("CONFIG");
+logger.info(ws_config);
 /*
   config example
 / - "CONFIG=[{ 'name': 'HHTs', 'port': 59999, 'lifesign': false, 'forward_topic': 'HHTMessages' }]"
 */
-var clientsHT = {}
+var clientsHT = {};
 ws_config.forEach(function (onews) {
   clientsHT[onews.name] = onews;
 });
@@ -34,18 +34,18 @@ var mqttclient = mqtt.connect({
   host: process.env.AMQC_URL,
   port: process.env.AMQC_PORT,
   username: process.env.AMQC_LOGIN,
-  password: process.env.AMQC_PASSWORD
-})
+  password: process.env.AMQC_PASSWORD,
+});
 
-logger.info("Starting MQTT:" + process.env.AMQC_URL + ". Listening to:" + process.env.IN_TOPIC)
+logger.info("Starting MQTT:" + process.env.AMQC_URL + ". Listening to:" + process.env.IN_TOPIC);
 
-mqttclient.on('connect', function () {
+mqttclient.on("connect", function () {
   mqttclient.subscribe(process.env.IN_TOPIC, function (err) {
-    logger.info("MQTT Connected")
-  })
-})
+    logger.info("MQTT Connected");
+  });
+});
 
-mqttclient.on('message', function (topic, message) {
+mqttclient.on("message", function (topic, message) {
   // message is Buffer
   messages++;
   logger.info("Received:" + message.toString());
@@ -75,18 +75,15 @@ mqttclient.on('message', function (topic, message) {
     onews = clientsHT[newmes.target];
     if (onews.wss.clients.size > 0) {
       onews.wss.clients.forEach(function (onecli) {
-        onecli.send(JSON.stringify({ "type": type, "data": newmes.data }));
+        onecli.send(JSON.stringify({ type: type, data: newmes.data }));
       });
+    } else {
+      logger.error("No client connected.");
     }
-    else {
-      logger.error("No client connected.")
-    }
-  }
-  catch
-  {
+  } catch {
     logger.error("Unable to decode message");
   }
-})
+});
 
 function moduleLifeSign() {
   var clients = [];
@@ -95,47 +92,64 @@ function moduleLifeSign() {
     if (onews.wss != undefined) {
       if (onews.wss.clients != undefined) {
         connections += onews.wss.clients.size;
-        clients.push({ "name": onews.name, "port": onews.port, "clients": +onews.wss.clients.size });
-      }
-      else {
-        clients.push({ "name": onews.name, "port": onews.port, "clients": 0 });
+        clients.push({ name: onews.name, port: onews.port, clients: +onews.wss.clients.size });
+      } else {
+        clients.push({ name: onews.name, port: onews.port, clients: 0 });
       }
     }
   });
   var lifesign = {
-    "error": "OK", "type": "lifesign", "module": "WebSocketServer", "version": VERSION, "alive": 1, "errors": 0, "internalerrors": 0, "heartbeaterrors": 0, "eventtype": "lifesign"
-    , "messages": messages, "received": {}, "amqclientversion": "1.0.0", "starttimets": new Date().getTime(), "starttime": new Date(), "connections": connections, "platform": process.platform
-    , "icon": "socks","clients":clients
+    error: "OK",
+    type: "lifesign",
+    module: "WebSocketServer",
+    version: VERSION,
+    alive: 1,
+    errors: 0,
+    internalerrors: 0,
+    heartbeaterrors: 0,
+    eventtype: "lifesign",
+    messages: messages,
+    received: {},
+    amqclientversion: "1.0.0",
+    starttimets: new Date().getTime(),
+    starttime: new Date(),
+    connections: connections,
+    platform: process.platform,
+    icon: "socks",
+    clients: clients,
   };
 
-  mqttclient.publish('NYX_MODULE_INFO', JSON.stringify(lifesign));
+  mqttclient.publish("NYX_MODULE_INFO", JSON.stringify(lifesign));
 }
 
 setInterval(moduleLifeSign, 5000);
 
 ws_config.forEach(function (onews) {
   logger.info(onews);
-  onews.wss = new WebSocket.Server({ port: onews.port })
-  onews.wss.on('connection', (ws,req) => {
-    logger.info(">> New Client. for "+onews.name+" port:"+onews.port)
-    logger.info(">> IP:"+req.connection.remoteAddress)
-    mqttclient.publish('NYX_WS_INFO', JSON.stringify({"Name":onews.name,"Port":onews.port,"IP":req.connection.remoteAddress}));
-    ws.on('message', (message) => {
+  onews.wss = new WebSocket.Server({ port: onews.port });
+  onews.wss.on("connection", (ws, req) => {
+    logger.info(">> New Client. for " + onews.name + " port:" + onews.port);
+    logger.info(">> IP:" + req.connection.remoteAddress);
+    mqttclient.publish(
+      "NYX_WS_INFO",
+      JSON.stringify({ Name: onews.name, Port: onews.port, IP: req.connection.remoteAddress })
+    );
+    ws.on("message", (message) => {
       logger.info(`Received message => ${message}`);
-      if ('forward_topic' in onews) {
-        logger.info(`Forwarding to topic => ${onews['forward_topic']}`);
-        mqttclient.publish(onews['forward_topic'], message);
+      if ("forward_topic" in onews) {
+        logger.info(`Forwarding to topic => ${onews["forward_topic"]}`);
+        mqttclient.publish(onews["forward_topic"], message);
       }
-    })
-  })
+    });
+  });
 });
 
 function checkWebSockets() {
   ws_config.forEach(function (onews) {
     logger.info("Name:" + onews.name + " clients:" + onews.wss.clients.size);
-    if (onews.wss.clients.size > 0 && (('lifesign' in onews && onews['lifesign'] == true) || !('lifesign' in onews))) {      
+    if (onews.wss.clients.size > 0 && (("lifesign" in onews && onews["lifesign"] == true) || !("lifesign" in onews))) {
       onews.wss.clients.forEach(function (onecli) {
-        onecli.send(JSON.stringify({ "type": "lifesign" }));
+        onecli.send(JSON.stringify({ type: "lifesign" }));
       });
     }
   });
